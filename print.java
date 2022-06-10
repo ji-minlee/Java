5127477079e59d6acd48526c62fb85405305d206
ji-minlee <106597633+ji-minlee@users.noreply.github.com>
2022-06-10 22:20:49 +0900

    java 1강

---
+++ print.java
@@ -0,0 +1,11 @@
+﻿package java001;
+
+public class P10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("kim\nlee\njohn\nmike\ndaddy");
+	}
+
+}
