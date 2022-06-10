7b5a86b1b4233162682c285c174cc55eabbefd3f
ji-minlee <106597633+ji-minlee@users.noreply.github.com>
2022-06-10 22:22:36 +0900

    java 1강

---
+++ java001/input.java
@@ -0,0 +1,24 @@
+﻿package java001;
+
+import java.util.Scanner;
+
+public class P12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Input the 1st number!");
+		int input1 = scan.nextInt();		//int > nextInt
+		
+		System.out.println("Input the 2nd number!");
+		int input2 = scan.nextInt();
+		
+		int sum = input1 + input2;
+		
+		System.out.printf("Result : %d + %d = %d", input1, input2, sum);
+	}
+
+}
